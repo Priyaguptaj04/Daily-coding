dbe2a721f3344f6b09548062e92a3be59a2ceb2b
priya <priyahj2004@gmail.com>
2025-07-02 19:13:31 +0530

    initial commit

--- code1.py
+++ code1.py
@@ -1,5 +1,4 @@
 sum = 0
 n = int(input("Enter the number: "))  
-for i in range(1, n + 1):            
-    sum += i                         
+n(n+1)/2                        
 print("The sum of numbers from 1 to", n, "is", sum)
